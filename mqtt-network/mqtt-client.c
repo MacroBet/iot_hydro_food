267f7d513ee8be2b92e38f6f14ddc25732d8db6b
MarcoRalli <marco_ralli@outlook.com>
2022-07-16 12:05:33 +0200

    mqtt-network

--- mqtt-network/mqtt-client.c
+++ mqtt-network/mqtt-client.c
@@ -282,11 +282,11 @@ PROCESS_THREAD(mqtt_client_process, ev, data)
 			if(watering){
           unsigned short r = random_rand() ;
           int vart = (int) r;
-          temperature += vart;
+          temperature += (vart%3);
           unsigned short r1 = random_rand();
           int varh = (int) r1;
           //varHum = r1%3;
-          humidity += varh;
+          humidity += (varh%3);
          
 
       }else {
