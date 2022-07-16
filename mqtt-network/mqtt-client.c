95f83f0a573f79755daf55575900e9c7b6f08d2f
MarcoRalli <marco_ralli@outlook.com>
2022-07-16 12:29:16 +0200

    mqtt-network

--- mqtt-network/mqtt-client.c
+++ mqtt-network/mqtt-client.c
@@ -122,11 +122,13 @@ PROCESS(mqtt_client_process, "MQTT Client");
 
 static int temperature = 25;
 static int humidity = 50;
-//static int co2 = 1400;
+static int co2 = 1400;
 static int varTemp = 0;
 static int varHum = 0;
+static int varCo2 = 0;
 unsigned short r1 = 0;
 unsigned short r = 0;
+unsigned short r2 = 0;
 static bool watering = true;
 
 
@@ -283,13 +285,15 @@ PROCESS_THREAD(mqtt_client_process, ev, data)
 			
 			if(watering){
           r = random_rand() ;
-          varTemp = ((int) r) % 3;
+          varTemp += ((int) r) % 3;
           temperature = varTemp;
           r1 = random_rand();
           varHum = ((int) r1) % 3;
-          //varHum = r1%3;
-          humidity = varHum;
-         
+          humidity += varHum;
+          r2 = random_rand();
+          varCo2 = ((int) r2) % 50;
+          co2 += varCo2;
+
 
       }else {
           
@@ -301,7 +305,8 @@ PROCESS_THREAD(mqtt_client_process, ev, data)
       }
   
       LOG_INFO("New val tem: %d\n", temperature);
-      sprintf(app_buffer, "{\"node\": %d, \"temperature\": %d, \"humidity\": %d}", node_id, temperature, humidity);
+      sprintf(app_buffer, "{\"node\": %d, \"temperature\": %d, \"humidity\": %d}, \"Co2\": %d}", 
+                            node_id, temperature, humidity, co2);
       mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer,
                strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
 		
