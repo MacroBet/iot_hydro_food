b1cd501b31383c6d2b714d689102c9bbe4a9518d
MarcoRalli <marco_ralli@outlook.com>
2022-07-16 11:50:29 +0200

    mqtt-network

--- mqtt-network/mqtt-client.c
+++ mqtt-network/mqtt-client.c
@@ -123,8 +123,8 @@ PROCESS(mqtt_client_process, "MQTT Client");
 static int temperature = 25;
 static int humidity = 50;
 //static int co2 = 1400;
-static int varTemp = 0;
-static int varHum = 0;
+//static int varTemp = 0;
+//static int varHum = 0;
 static bool watering = true;
 
 
@@ -280,11 +280,11 @@ PROCESS_THREAD(mqtt_client_process, ev, data)
 		  sprintf(pub_topic, "%s", "status");
 			
 			if(watering){
-          unsigned short r = random_rand() ;
-          varTemp = r%3;
+          unsigned short r = random_rand()%3 ;
+          //varTemp = r%3;
           temperature += r;
-          unsigned short r1 = random_rand();
-          varHum = r1%3;
+          unsigned short r1 = random_rand()%3;
+          //varHum = r1%3;
           humidity += r1;
          
 
