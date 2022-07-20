cb0c7f6de378c8c932f685b77b3cc995b4dd6404
MarcoRalli <marco_ralli@outlook.com>
2022-07-20 21:49:37 +0200

    shell

--- controller/main.py
+++ controller/main.py
@@ -1,4 +1,5 @@
 
+from pydoc import cli
 import threading
 import time
 from mqttNetwork.mqtt_collector import MqttClient
@@ -24,7 +25,8 @@ def checkCommand(command, client):
         print(client.message)
         try:
             while True:
-                if(mex == client.message):
+                if(mex != client.message):
+                    print("Press ctrl + C to exit \n")
                     print(client.message)
         except KeyboardInterrupt:
          return
@@ -32,11 +34,53 @@ def checkCommand(command, client):
     elif command == "!change values of thresholds":
            print("......")
     elif command == "!check values sensors co2":
-            print("......")
+
+        while 1:
+            client.co2Max = input("Insert new Max value for co2: ")
+            client.co2Min = input("Insert new Min value for co2: ")
+            print("New max/min value for co2 are : "+ client.co2Max +", " + client.co2Min)
+            print("Do you want continue with this value[y/n]?")
+            answer = input(">")
+            answer = answer.lower()
+            if(answer == "yes" or answer == "y"):
+                print("Value saved\n")
+                break
+            elif(answer == "no" or answer == "n"):
+                print("Insert new value\n")
+                continue
+
     elif command == "!check values sensors humidity":
-           print("......")
+          
+           while 1:
+            client.co2Max = input("Insert new Max value for humidity: ")
+            client.co2Min = input("Insert new Min value for humidity: ")
+            print("New max/min value for co2 are : "+ client.humMax +", " + client.humMin)
+            print("Do you want continue with this value[y/n]?")
+            answer = input(">")
+            answer = answer.lower()
+            if(answer == "yes" or answer == "y"):
+                print("Value saved\n")
+                break
+            elif(answer == "no" or answer == "n"):
+                print("Insert new value\n")
+                continue
+
     elif command == "!check values sensors temperature":
-            print("......")
+            
+            while 1:
+                client.co2Max = input("Insert new Max value for temperature: ")
+                client.co2Min = input("Insert new Min value for temprature: ")
+                print("New max/min value for co2 are : "+ client.tempMax +", " + client.tempMin)
+                print("Do you want continue with this value[y/n]?")
+                answer = input(">")
+                answer = answer.lower()
+                if(answer == "yes" or answer == "y"):
+                    print("Value saved\n")
+                    break
+                elif(answer == "no" or answer == "n"):
+                    print("Insert new value\n")
+                    continue
+
     elif command == "!list of commands":
             listOfcommands()
     else:
@@ -56,28 +100,42 @@ def showInfo():
 
 if __name__ == "__main__":
 
-    print("Define tresholds for the parameters Temperature, Humidity, Co2 :\n")
-    tempMax = input("TRESHOLD MAX TEMPERATURE  (default value 35C) : ")
-    if tempMax == "":
-        tempMax =  35 
-    tempMin = input("TRESHOLD MIN TEMPERATURE  (default value 20C) : ")
-    if tempMin == "":
-        tempMin = 20 
-    humMax = input("TRESHOLD MAX HUMIDITY %  (defalut value 80%) : ")
-    if humMax == "":
-        humMax = 80 
-    humMin = input("TRESHOLD MIN HUMIDITY %  (default value 30%) : ")
-    if humMin == "":
-        humMin = 35 
-    co2Max = input("TRESHOLD MAX CO2  (default value 2000ppm) : ")
-    if co2Max == "": 
-        co2Max = 2000
-    co2Min = input("TRESHOLD MIN CO2  (default value 1000ppm) : ")
-    if co2Min == "":
-        co2Min = 1000
-    print("\nValues for tresholds: \n Max Temperature = " + str(tempMax) + ",\n Min Temperature = " + str(tempMin) + " ,\n Max Humidity = " + str(humMax) + ",\n Min Humidity = " + str(humMin) +",\
-                \n Max Co2 = "+ str(co2Max) +", \n Min Co2 = "+ str(co2Min)+"\n\n")
-    
+    while 1:
+        print("Define tresholds for the parameters Temperature, Humidity, Co2 :\n")
+        tempMax = input("TRESHOLD MAX TEMPERATURE  (default value 35C) : ")
+        if tempMax == "":
+            tempMax =  35 
+        tempMin = input("TRESHOLD MIN TEMPERATURE  (default value 20C) : ")
+        if tempMin == "":
+            tempMin = 20 
+        humMax = input("TRESHOLD MAX HUMIDITY %  (defalut value 80%) : ")
+        if humMax == "":
+            humMax = 80 
+        humMin = input("TRESHOLD MIN HUMIDITY %  (default value 30%) : ")
+        if humMin == "":
+            humMin = 35 
+        co2Max = input("TRESHOLD MAX CO2  (default value 2000ppm) : ")
+        if co2Max == "": 
+            co2Max = 2000
+        co2Min = input("TRESHOLD MIN CO2  (default value 1000ppm) : ")
+        if co2Min == "":
+            co2Min = 1000
+        
+        print("You choose these values:\n")
+        print("\nValues for tresholds: \n Max Temperature = " + str(tempMax) + ",\n Min Temperature = " \
+            + str(tempMin) + " ,\n Max Humidity = " + str(humMax) + ",\n Min Humidity = " + str(humMin) +",\
+                    \n Max Co2 = "+ str(co2Max) +", \n Min Co2 = "+ str(co2Min)\
+                    +"\n\nDo you want to continue with this values [y/n]?")
+        
+        answer = input(">")
+        answer = answer.lower()
+        if(answer == "yes" or answer == "y"):
+            print("Welcome\n")
+            break
+        elif(answer == "no" or answer == "n"):
+            print("Insert new value\n")
+            continue
+
     listOfcommands()
 
     print("System is going to start----->\n")
