db16a17e099da4b31e62c77d5fc36ac31920d03f
MarcoRalli <marco_ralli@outlook.com>
2022-07-23 17:50:24 +0200

    ok

--- examples/coap_example/coap-example-client.c
+++ examples/coap_example/coap-example-client.c
@@ -87,7 +87,7 @@ PROCESS_THREAD(er_example_client, ev, data)
       printf("--Toggle timer--\n");
 
       /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
-      coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
       coap_set_header_uri_path(request, "hello/");
 
       const char msg[] = "Toggle!";
