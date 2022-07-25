b6eadfd8940b5ca73b412ed8e885adac48a8876f
MarcoRalli <marco_ralli@outlook.com>
2022-07-25 11:54:55 +0200

    observing

---
+++ examples/coap_example/iot-coap-python/resExample.py
@@ -0,0 +1,33 @@
+import getopt
+import sys
+from coapthon.server.coap import CoAP
+from coapthon.resources.resource import Resource
+from coapthon.messages.request import Request
+from coapthon.messages.response import Response
+from coapthon.client.helperclient import HelperClient
+from coapthon.server.coap import CoAP
+from obs_sensor import ObserveSensor
+from coapthon.resources.resource import Resource
+from addresses import Addresses
+from server import *
+
+class ResExample(Resource):
+    
+    mote = 0
+    address = None
+    def __init__(self, name="ResExample", coap_server=None):
+        super(ResExample, self).__init__(name, coap_server, visible=True, observable=True, allow_children=True)
+
+        self.payload = "Basic Resource"
+        self.resource_type = "rt1"
+        self.content_type = "text/plain"
+        self.interface_type = "if1"
+
+    def render_GET(self, request):
+        Addresses.insertNewAddress(request.source)
+        ResExample.mote = 1
+        print(request.source)
+        return self
+
+    def checkpresence():
+        return ResExample.mote
--- examples/coap_example/iot-coap-python/server.py
+++ examples/coap_example/iot-coap-python/server.py
@@ -11,21 +11,9 @@ from coapthon.server.coap import CoAP
 from obs_sensor import ObserveSensor
 from coapthon.resources.resource import Resource
 from addresses import Addresses
-class ResExample(Resource):
-    
-    address = None
-    def __init__(self, name="ResExample", coap_server=None):
-        super(ResExample, self).__init__(name, coap_server, visible=True, observable=True, allow_children=True)
+from resExample import ResExample
 
-        self.payload = "Basic Resource"
-        self.resource_type = "rt1"
-        self.content_type = "text/plain"
-        self.interface_type = "if1"
 
-    def render_GET(self, request):
-        #Addresses.insertNewAddress(request.source)
-        print(request.source)
-        return self
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
@@ -36,22 +24,19 @@ class CoAPServer(CoAP):
 ip = "::"
 port = 5683
 
-
 server = CoAPServer(ip, port)
-
-
-
 
 try:
     server.listen(10)
     while(1) :
-        add = Addresses.constructAddress()
-        if add is not None :
-            for address in add :
-                print(address)
-                client = HelperClient(address)
-                path="status"
-                response = client.post("obs", "mode=0")
+        if ResExample.checkpresence:
+            add = Addresses.constructAddress()
+            if add is not None :
+                for address in add :
+                    print(address)
+                    client = HelperClient(address)
+                    path="status"
+                    response = client.post("obs", "mode=0")
     
 
 except KeyboardInterrupt:
