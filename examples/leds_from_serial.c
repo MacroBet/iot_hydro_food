242918ccfd5a2631a663e2b02906e3e42baeb195
MarcoRalli <marco_ralli@outlook.com>
2022-07-28 17:44:33 +0200

    led test

--- examples/leds_from_serial.c
+++ examples/leds_from_serial.c
@@ -32,7 +32,7 @@
 
 #include "contiki.h"
 #include "dev/leds.h"
- #include  
+
 PROCESS(hello_world_process, "Hello world process"); AUTOSTART_PROCESSES(&hello_world_process); 
 PROCESS_THREAD(hello_world_process, ev, data) { 
 	PROCESS_EXITHANDLER(); 
