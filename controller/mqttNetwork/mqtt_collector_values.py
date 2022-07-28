64678221ce5c05992f08aa84bd15dce073a92f1b
MarcoRalli <marco_ralli@outlook.com>
2022-07-28 10:41:42 +0200

    temperature outside

--- controller/mqttNetwork/mqtt_collector_values.py
+++ controller/mqttNetwork/mqtt_collector_values.py
@@ -159,7 +159,7 @@ class MqttClientData:
 
     def executeLastState(self, address, table) :
         cursor = self.connection.cursor()
-        sql = "SELECT status FROM actuator_{}".format(table) + "WHERE address = %s ORDER BY timestamp DESC LIMIT 1"
+        sql = "SELECT status FROM actuator_"+table+ " WHERE address = %s ORDER BY timestamp DESC LIMIT 1"
         cursor.execute(sql, str(address))
         result_set = cursor.fetchall()
         if not result_set :
