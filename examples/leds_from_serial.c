2c7a480c307d7fe799508521741914f535a28728
MarcoRalli <marco_ralli@outlook.com>
2022-07-29 11:02:42 +0200

    led test

--- examples/leds_from_serial.c
+++ examples/leds_from_serial.c
@@ -55,7 +55,14 @@ PROCESS_THREAD(leds_example, ev, data)
     PROCESS_YIELD();
 
     if(ev == PROCESS_EVENT_TIMER && data == &et) {
-     
+      if((counter & 7) == 0) {
+        leds_set(LEDS_ALL);
+      } else if((counter & 7) == 1) {
+        leds_off(LEDS_ALL);
+      } else if((counter & 7) == 2) {
+        leds_on(LEDS_LED1);
+      } else if((counter & 7) == 3) {
+        leds_toggle(LEDS_ALL);
 #if !LEDS_LEGACY_API
       } else if((counter & 7) == 4) {
         leds_single_on(LEDS_LED1);
@@ -64,11 +71,13 @@ PROCESS_THREAD(leds_example, ev, data)
       } else if((counter & 7) == 6) {
         leds_single_toggle(LEDS_LED1);
 #endif /* LEDS_LEGACY_API */
-      } 
+      } else if((counter & 7) == 7) {
+        leds_toggle(LEDS_ALL);
+      }
 
       counter++;
       etimer_set(&et, CLOCK_SECOND);
-    
+    }
   }
 
   PROCESS_END();
