529d79482abd89e82eb61a2e6c64e76c54c319f3
MarcoRalli <marco_ralli@outlook.com>
2022-07-29 11:26:28 +0200

    led test

--- examples/leds_from_serial.c
+++ examples/leds_from_serial.c
@@ -56,8 +56,7 @@ PROCESS_THREAD(leds_example, ev, data)
 
     if(ev == PROCESS_EVENT_TIMER && data == &et) {
      
-       leds_on(LEDS_LED1);
-     
+       leds_set(LEDS_NUM_TO_MASK(LEDS_GREEN));
       
 
       counter++;
