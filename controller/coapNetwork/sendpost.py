b970068417a51d9dbe95b7126bccc49319884d17
MarcoRalli <marco_ralli@outlook.com>
2022-07-31 12:23:03 +0200

    led test

--- controller/coapNetwork/sendpost.py
+++ controller/coapNetwork/sendpost.py
@@ -11,7 +11,7 @@ class Post:
     def  changeStatusWatering(status, ad):
         client = HelperClient(ad)
         response = client.post("obs", "mode="+status)
-        print(response.status)
+        print(response.status_code)
         return response
 
 
