9701b5dfe16237cf35a040a756d1b83cea158262
Marco Bellia <ing.bellia@gmail.com>
2022-08-17 16:23:55 +0200

    123

--- scripts/add_data.sh
+++ scripts/add_data.sh
@@ -7,7 +7,7 @@ do
     temperature=$((20 + $RANDOM % 40))
     humidity=$((10 + $RANDOM % 90))
     co2=$((1400 + $RANDOM % 600))
-    mysql -uroot -pmypass -e "insert into iot.data (id_node,timestamp,temperature,humidity,co2) values ($id_node,$timestamp,$temperature,$humidity,$co2);"
+    mysql -uroot -pmypass123 -e "insert into iot.data (id_node,timestamp,temperature,humidity,co2) values ($id_node,$timestamp,$temperature,$humidity,$co2);"
     i=$(($i+1))
     sleep 6 
 done
