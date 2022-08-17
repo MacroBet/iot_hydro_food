d35f722b51c1a3945faaa3f5f4de44b800378ca2
Marco Bellia <ing.bellia@gmail.com>
2022-08-17 16:16:35 +0200

    update script

--- scripts/add_data.sh
+++ scripts/add_data.sh
@@ -2,12 +2,12 @@
 i=1
 while [ $i -le 100000000 ]
 do
-    id_node = $((1 + $RANDOM % 10))
+    id_node=$((1 + $RANDOM % 10))
     timestamp=$(date +%s)
-    temperature =  $((20 + $RANDOM % 40))
-    humidity =  $((10 + $RANDOM % 90))
-    co2 =  $((1400 + $RANDOM % 600))
-    mysql -uroot -pPASSWORD test -e "insert into iot.data (id_node,timestamp,temperature,humidity,co2) values ($id_node,$timestamp,$temperature,$humidity,$co2);"
+    temperature=$((20 + $RANDOM % 40))
+    humidity=$((10 + $RANDOM % 90))
+    co2=$((1400 + $RANDOM % 600))
+    mysql -uroot -pmypassword123 -e "insert into iot.data (id_node,timestamp,temperature,humidity,co2) values ($id_node,$timestamp,$temperature,$humidity,$co2);"
     i=$(($i+1))
     sleep 6 
 done
