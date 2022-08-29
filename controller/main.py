7a24326791365b1c68ce87c422305b711f897a22
Marco Bellia <ing.bellia@gmail.com>
2022-08-29 15:42:25 +0200

    cli fix

--- controller/main.py
+++ controller/main.py
@@ -178,7 +178,7 @@ def start_configuration():
 
 if __name__ == "__main__":
     cfg=  {"tempMax":35,"tempMin":20,"humMax":80,"humMin":35,"co2Max":2000,"co2Min":1000}
-    should_configure = input("Welcome to Idrofood simulator 🚀 do you want to configure parameters?").lower()
+    should_configure = input("Welcome to Idrofood simulator 🚀 \nDo you want to configure parameters? (y/n)").lower()
     if(should_configure == "yes" or should_configure == "y"):
         cfg = start_configuration()
 
