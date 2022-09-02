74068bc7eafca35ad659447ac0a711c77112d19a
MarcoRalli <marco_ralli@outlook.com>
2022-09-02 17:04:30 +0200

    test

--- controller/globaStatus.py
+++ controller/globaStatus.py
@@ -5,8 +5,8 @@ class globalStatus:
     statusValve = 0
     statusWindow = 0
 
-    def setStatusValve(self, status): self.statusValve = status
+    def setStatusValve(status): globalStatus.statusValve = status
 
-    def setStatusWindow(self, status): self.statusWindow = status
+    def setStatusWindow(status): globalStatus.statusWindow = status
 
     
