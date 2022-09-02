0c521cc0eadb8badd9ec7cef333cc472f9c1c478
MarcoRalli <marco_ralli@outlook.com>
2022-09-02 16:29:44 +0200

    test

--- controller/main.py
+++ controller/main.py
@@ -97,8 +97,8 @@ def checkCommand(command, client, client1):
             
             while True:
                 
-                statWat = client.stateWat if client.stateWat != None  else 0
-                statWind = client.stateWat if client.stateWat != None  else 0
+                statWat = client.stateWat if client.stateWat != None  else '0'
+                statWind = client.stateWat if client.stateWat != None  else '0'
                 data = extractValuesFromClient(client,client1)
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(pa.greenhouse[statWat][statWind].format(data[0],data[1],data[2],data[3],data[4]))
