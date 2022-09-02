c699af884060cfe233e91512513a4b3384908a28
MarcoRalli <marco_ralli@outlook.com>
2022-09-02 13:49:44 +0200

    last changes

--- controller/main.py
+++ controller/main.py
@@ -3,6 +3,7 @@ import json
 from pydoc import cli
 import threading
 import time
+import sys
 from coapNetwork.addresses import Addresses
 from mqttNetwork.mqtt_collector_values import MqttClientData
 from mqttNetwork.mqqt_collector_bath_float import MqttClientBathFloat
@@ -39,7 +40,10 @@ def listOfcommands():
         "log\n"\
         "bath\n"\
         "sim\n"\
+        "change val\n"
         "exit\n\n")
+
+
 
 def extractValuesFromClient(client,client1):
     
@@ -50,6 +54,8 @@ def extractValuesFromClient(client,client1):
     tempOut = client.tempOut if client.tempOut else 30
 
     return [temperature,tempOut,humidity,co2,level]
+
+
             
 
 def checkCommand(command, client, client1):
@@ -108,13 +114,23 @@ def checkCommand(command, client, client1):
         except KeyboardInterrupt:
             return
 
-       
+    elif command == "change val":
+
+        cfg = start_configuration()
+        client.tempMax = cfg["tempMax"]   
+        client.tempMin = cfg["tempMin"]
+        client.humMax = cfg["humMax"]
+        client.humMin = cfg["humMin"]
+        client.co2Max = cfg["co2Max"]
+        client.co2Min = cfg["co2Min"]
+
     elif command == "exit":
         thread.join()
         thread1.join()
         thread2.join()
         server.close()
         print("SHUTDOWN")
+        sys.exit()
     else:
         print("Command not found")
         listOfcommands()
@@ -123,19 +139,21 @@ def checkCommand(command, client, client1):
 
 def showInfo():
 
-    print("log ---> check message that the sensord send to the application \n"\
-          "change value co2 ---> chack only the values of Co2\n"\
-          "change value humidity ---> check only the values of humidity\n"\
-          "change value temperature ---> check only the values of temperature\n"\
-          "bath ---> check level of the bath float\n"\
-          "sim ---> show simulation of the greenhouse\n"\
-          "help ---> show the commands that can be promted\n")
+    print("log ---> to check message that the sensord send to the application \n"\
+          "change val ---> to change the current values of the tresholds\n"\
+          "bath ---> to check level of the bath float\n"\
+          "sim ---> to show simulation of the greenhouse\n"\
+          "activate ---> to activate all the sensors in the network\n"
+          "help ---> to show the commands that can be promted\n")
+
 
 
 def validate(field,defaultValue):
     if(field.isnumeric()):
         return int(field)
     return defaultValue
+
+
 
 def start_configuration():
 
@@ -157,6 +175,14 @@ def start_configuration():
         return {"tempMax":tempMax,"tempMin":tempMin,"humMax":humMax,"humMin":humMin,"co2Max":co2Max,"co2Min":co2Min}
     elif(answer == "no" or answer == "n"):
         return start_configuration()
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     cfg=  {"tempMax":35,"tempMin":20,"humMax":80,"humMin":35,"co2Max":1800,"co2Min":1200}
@@ -188,7 +214,7 @@ if __name__ == "__main__":
         while 1:
             if ResExample.valves == 1 and ResExample.windows == 1:
 
-                    print("System is running--->\n\n ")
+                    print("🟢🟢 System is running 🟢🟢\n\n ")
                     if start == 0:
                         print("Use command 'activate' to start sensor node\n\n ")
                         start = 1
@@ -198,7 +224,7 @@ if __name__ == "__main__":
                     checkCommand(command, client, client1)
             else:
                 
-                print("Controller is wating for all the sensors\n\n ")
+                print("⌛️⌛️⌛️ Controller is wating for all the sensors ⌛️⌛️⌛️\n\n ")
                 time.sleep(5)
 
         
@@ -208,4 +234,7 @@ if __name__ == "__main__":
         thread2.join()
         server.close()
         print("SHUTDOWN")
+
+
+
 
