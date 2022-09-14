f2741853789b19e3a1f470d2c09bdb21b09ab549
MarcoRalli <marco_ralli@outlook.com>
2022-09-14 10:45:56 +0200

    fix

--- scripts/test.py
+++ scripts/test.py
@@ -4,26 +4,29 @@ import sys
 import os
 import threading
 from time import sleep
+from wsgiref import validate
 
 
+def start_configuration():
+    print("Define tresholds for the parameters Temperature, Humidity, Co2 :\n")
+    tempMax = validate(input("TRESHOLD MAX TEMPERATURE  (default value 35C) : "),35)
+    tempMin = validate(input("TRESHOLD MIN TEMPERATURE  (default value 20C) : "),20)
+    humMax = validate(input("TRESHOLD MAX HUMIDITY %  (defalut value 80%) : "),80)
+    humMin = validate(input("TRESHOLD MIN HUMIDITY %  (default value 30%) : "),35)
+    co2Max = validate(input("TRESHOLD MAX CO2  (default value 2000ppm) : "),2000)
+    co2Min = validate(input("TRESHOLD MIN CO2  (default value 1000ppm) : "),1000)
+    
+    print("You choose these values:\n")
+    print("\nValues for tresholds: \n Max Temperature = {},\n Min Temperature = {},\n Max Humidity = {},\n Min Humidity = {}, \n Max Co2 ={},\n Min Co2 = {}".format( str(tempMax), str(tempMin),str(humMax), str(humMin),str(co2Max),str(co2Min) ))
+    print("\nDo you want to continue with this values [y/n]?")
+    
+    answer = input(">")
+    answer = answer.lower()
+    if(answer == "yes" or answer == "y"):
+        return {"tempMax":tempMax,"tempMin":tempMin,"humMax":humMax,"humMin":humMin,"co2Max":co2Max,"co2Min":co2Min}
+    elif(answer == "no" or answer == "n"):
+        return start_configuration()
 
-greenhouse =  """"
-⠀⠀⠀⠀⠀⠀⠀⠀
-  ⠀ ⠀⠀⠀⠀⣠⠀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣠⡞⠛⢿⣍⠉⠉⠉⢻⣏⠉⠉⠙⢷⡄⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀  💨⠀ ⠙⢷⡄⠀  💨
-⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠈⠻⣦⡀ ⠀⠻⣦⠀⠀⠈⠻⣦⠀⠀⠀⠀    
-⠀⠀⢀⣴⠟⠀⠀⠀⠀⠀      💨   ⠙⢷⣄⠀  💨
-⠀⢰⡟⠁⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠙⢷⡤⠤⠤⠿⣦⠤⠤⠼⢿⡆⠀
-⠀⢸⡇    HYDRO FOOD   ⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇
-⠀⢸⡇                 ⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇   🌡 {}° / {}°
-⠀⢸⡇                 ⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇   💦 {}%
-⠀⢸⡇    🌱🌱🌱🌱🌱   ⢸⠗⠒⠒⠒⣿⠒⠒⠒⢺⡇⠀ 💨 {}ppm
-⠀⢸⡇⠀                ⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇
-⠀⢸⡇    🌱🌱🌱🌱🌱   ⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇   🛁 {}%
-⠀⢸⡇⠀              🚰⢸⠀⠀⠀⠀⣿⠀⠀⠀⢸⡇
-⠀⢸⣇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀ 🛁🛁🛁⣸⣀⣀⣀⣀⣿⣀⣀⣀⣸⡇⠀
-⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀
-"""
 
-print(greenhouse)
+cfg = start_configuration()
+print(cfg["tempMax"])
